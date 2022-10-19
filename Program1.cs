9b615c18f1b2f7c61a20c1acd38b5378993987c9
TsysAnastasiia <112326472+TsysAnastasiia@users.noreply.github.com>
2022-10-19 11:20:22 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class Person
+    {
+        string name;
+        int age;
+        public Person(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Person(string name)");
+        }
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+            Console.WriteLine("Person(string name, int age)");
+        }
+    }
+
+    class Employee : Person
+    {
+        string company;
+        public Employee(string name, int age, string company)
+            : base(name, age)
+        {
+            this.company = company;
+            Console.WriteLine("Employee(string name, int age, string company)");
+        }
+        }
+}
